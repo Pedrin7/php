4cb83c1f85bf43bb6f2c72c5dea2ccb628b6ae3a
Pedrin7 <pedropaim987@gmail.com>
2025-06-01 20:12:11 -0300

    vendo os tipos de variáveis em PHP

--- php-sandbox-starter/01-variables-data-types/03-data-types/index.php
+++ php-sandbox-starter/01-variables-data-types/03-data-types/index.php
@@ -1,14 +1,41 @@
 <?php
 
-/*
-  PHP DATA TYPES:
 
-- String
-- Integer
-- Float
-- Boolean
-- Array
-- Object
-- NULL
-- Resource
-*/
+// PHP DATA TYPES:
+
+// - String
+$name = 'Pedro Paim';
+$name2 = "pedro paim 2";
+var_dump($name2);
+echo '<br />';
+echo gettype($name);
+echo '<br />';
+
+// - Integer
+$age = 23;
+var_dump($age);
+echo '<br />';
+
+//- Float
+$nota = 7.5;
+var_dump($nota);
+echo '<br />';
+
+// - Boolean
+$carregou = true;
+var_dump($carregou);
+echo '<br />';
+
+// - Array
+$friends = ['pedro', 'maya', 80];
+var_dump($friends);
+echo '<br />';
+
+// - Object
+$pessoa = new stdClass();
+var_dump($pessoa);
+echo '<br />';
+
+// - NULL
+$car = null;
+var_dump($car);
