bf3e313ddeae1f97f43b9f8c60ac0e749f59ed49
Pedrin7 <pedropaim987@gmail.com>
2025-06-04 10:42:31 -0300

    funções para trabalhar com strings

--- php-sandbox-starter/01-variables-data-types/08-string-functions/index.php
+++ php-sandbox-starter/01-variables-data-types/08-string-functions/index.php
@@ -1,5 +1,38 @@
 <?php
 $output = null;
+
+$string = 'Hello world';
+
+//strlen
+$output = strlen($string);//retorna o tamanho da string
+
+//str_world_count
+$output = str_word_count($string);//retorna a quantidade de palavras
+
+//strpos
+$output = strpos($string, 'world');//retorna o index onde a palavra passada no segundo argumento começa
+
+//Pegando um index em especifico
+$output = $string[6];//retorna um char do index informado
+
+//substr
+$output = substr($string, 6, 5);//retorna uma parte de uma string, começando de um determinado índice.
+
+//str_replace
+$output = str_replace('world', 'Universe',$string);//substitui a palavra informada
+
+//strtolower
+$output = strtolower($string);//retorna toda a string em letras minusculas
+
+// strtoupper
+$output = strtoupper($string);//retorna toda a string em letras maiusculas
+
+// ucwords
+$output = ucwords($string);//a primeira letra de cada palavra retorna em letra maiuscula
+
+$output = trim('            hello         ');//remove os espaços
+
+
 ?>
 
 <!DOCTYPE html>
