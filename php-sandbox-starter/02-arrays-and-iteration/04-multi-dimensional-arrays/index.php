afcf266f1fe0d26e003f3c7b2144edd59bb5ce04
Pedrin7 <pedropaim987@gmail.com>
2025-06-12 16:48:07 -0300

    arrays multidimensionais

--- php-sandbox-starter/02-arrays-and-iteration/04-multi-dimensional-arrays/index.php
+++ php-sandbox-starter/02-arrays-and-iteration/04-multi-dimensional-arrays/index.php
@@ -1,5 +1,35 @@
 <?php
 $output = null;
+
+$fruits = [
+  ['Apple', 'Red'],
+  ['Orange', 'orange'],
+  ['Banana', 'yellow']
+];
+
+$fruits[] = ['Lemon', 'Green'];
+
+// $output = $fruits[3][0];
+
+$users = [
+  ['name' => 'Pedro',
+  'email' => 'teste@teste.com',
+  'senha' => 123456],
+
+  ['name' => 'Maya',
+  'email' => 'teste@maya.com',
+  'senha' => 123],
+
+  ['name' => 'mari',
+  'email' => 'teste@mari.com',
+  'senha' => 456]
+];
+
+$users[] = ['name' => 'teste', 'email' => 'teste@teste2.com', 'senha' => 789];
+
+array_push($users, ['name' => 'teste3', 'email' => 'teste@teste3.com', 'senha' => 987]);
+
+$output = count($users);
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +52,19 @@ $output = null;
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
       <!-- Output -->
       <p class="text-xl"><?= $output ?></p>
+      <h1>array das frutas</h1>
+      <p>
+        <pre>
+          <?php var_dump($fruits) ?>
+        </pre>
+      </p>
+
+      <h1>array dos usuários</h1>
+      <p>
+        <pre>
+          <?php var_dump($users)?>
+        </pre>
+      </p>
     </div>
   </div>
 </body>
