e513bcc1f52d53c5bca994768fdd4fee5ee4e25c
Pedrin7 <pedropaim987@gmail.com>
2025-06-16 17:18:00 -0300

    loops basicos em PHP

--- php-sandbox-starter/02-arrays-and-iteration/06-basic-loops/index.php
+++ php-sandbox-starter/02-arrays-and-iteration/06-basic-loops/index.php
@@ -1,3 +1,27 @@
+<?php
+
+//Basic for loop
+// for($i = 0; $i <= 10; $i++){
+//     echo $i . '<br />';
+// }
+
+//basic while loop
+// $i = 0
+// while($i < 10){
+//     echo $i . '<br />';
+//     $i++;
+// }
+
+//basic do while
+// $i = 0;
+// do{
+//     echo $i;
+//     $i++;
+// }while($i <= 10)
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,6 +41,11 @@
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
+             <ul>
+                <?php $i = 0; do{?>
+                    <li>Numeros: <?= $i . '<br />';?></li>
+                <?php $i++;} while($i < 10) ?>
+             </ul>
         </div>
     </div>
 </body>
