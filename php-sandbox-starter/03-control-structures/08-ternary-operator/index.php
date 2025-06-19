805b66cf3516b1d9893c44e3c8d004db632d3c8b
Pedrin7 <pedropaim987@gmail.com>
2025-06-19 20:21:53 -0300

    operador ternário

---
+++ php-sandbox-starter/03-control-structures/08-ternary-operator/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$score = 50;
+
+if($score > 40){
+    echo 'high score!';
+}else{
+    echo 'bad score';
+}
+
+// exemplo de operador ternário
+echo $score > 40 ? 'High score' : 'Low score';
