4152b867d4f2fb5fdedfbd909c7a1e048b150107
Pedrin7 <pedropaim987@gmail.com>
2025-06-19 14:47:01 -0300

    começando o desafio fizzbuzz

--- php-sandbox-starter/03-control-structures/06-fizzbuzz-challenge/index.php
+++ php-sandbox-starter/03-control-structures/06-fizzbuzz-challenge/index.php
@@ -1,5 +1,25 @@
 <?php
 /*
 Fizzbuzz Challenge:
-Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”. Remember, you can use the modulus operator to check if a number is divisible by another number.
+Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print “Fizz” instead of the number and 
+for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five print “FizzBuzz”. 
+Remember, you can use the modulus operator to check if a number is divisible by another number.
 */
+
+for($i = 1; $i <= 100; $i++){
+    if($i % 3 === 0){
+        echo $i . ' Fizz <br>';
+    }elseif($i % 5 === 0) {
+        echo $i . ' Buzz <br>';
+    }elseif($i % 3 === 0 && $i % 5 === 0){
+        echo $i . ' FizzBuzz <br>';
+    }else{
+        echo $i . '<br>';
+    }
+
+}
+
+
+?>
