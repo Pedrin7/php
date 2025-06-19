5c52ba5429418e7ba148b59b2d73b9c48bf8eb9e
Pedrin7 <pedropaim987@gmail.com>
2025-06-19 18:53:38 -0300

    finalizando o desafio fizzbuzz!

--- php-sandbox-starter/03-control-structures/06-fizzbuzz-challenge/index.php
+++ php-sandbox-starter/03-control-structures/06-fizzbuzz-challenge/index.php
@@ -9,12 +9,12 @@ Remember, you can use the modulus operator to check if a number is divisible by 
 */
 
 for($i = 1; $i <= 100; $i++){
-    if($i % 3 === 0){
-        echo $i . ' Fizz <br>';
-    }elseif($i % 5 === 0) {
-        echo $i . ' Buzz <br>';
-    }elseif($i % 3 === 0 && $i % 5 === 0){
-        echo $i . ' FizzBuzz <br>';
+    if($i % 3 === 0 && $i % 5 === 0){
+        echo 'FizzBuzz <br>';
+    }elseif($i % 3 === 0 ) {
+        echo 'Fizz <br>';
+    }elseif($i % 5 === 0){
+        echo 'Buzz <br>';
     }else{
         echo $i . '<br>';
     }
