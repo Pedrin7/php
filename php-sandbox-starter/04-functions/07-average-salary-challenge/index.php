c6455375f550ec8f4d239b9890e16e42ded50727
Pedrin7 <pedropaim987@gmail.com>
2025-06-24 11:17:32 -0300

    começando o exercicio ed nomes

--- php-sandbox-starter/04-functions/07-average-salary-challenge/index.php
+++ php-sandbox-starter/04-functions/07-average-salary-challenge/index.php
@@ -52,6 +52,17 @@ function highlightTags($tags, $searchTerm)
   $tagsArray = implode(', ', $tags);
   return str_replace($searchTerm, "<span class='bg-yellow-200'>$searchTerm</span>", $tagsArray);
 }
+
+function calculateAverageSalary($joblistings) {
+  $cont = 0;
+  $sumSalaries = 0;
+
+  foreach($joblistings as $job){
+    $sumSalaries += $job['salary'];
+    $cont ++;
+  }
+  // return formatSalary($sumSalaries);
+}
 ?>
 
 
@@ -73,7 +84,7 @@ function highlightTags($tags, $searchTerm)
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-green-100 rounded-lg shadow-md p-6 my-6">
-      <h2 class="text-2xl font-semibold mb-4">Average Salary:</h2>
+      <h2 class="text-2xl font-semibold mb-4">Average Salary: <?= calculateAverageSalary()?> </h2>
     </div>
     <!-- Output -->
     <?php foreach ($listings as $index => $job) : ?>
