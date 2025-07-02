c045467884d92e6ba1708365f9721c8c679a9e42
Pedrin7 <pedropaim987@gmail.com>
2025-07-02 18:07:58 -0300

    erança

--- php-sandbox-starter/05-oop/03-inheritance/index.php
+++ php-sandbox-starter/05-oop/03-inheritance/index.php
@@ -17,3 +17,25 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+
+class Admin extends User{
+  public $level;
+
+  public function __construct($name, $email, $level){
+
+    $this->level = $level;
+    parent::__construct($name, $email);
+  }
+
+  public function getStatus(){
+    echo $this->status;
+  }
+
+}
+
+$admin1 = new Admin('Pedro', 'teste@teste.com', 5);
+
+// var_dump($admin1);
+$admin1->getStatus();
+
