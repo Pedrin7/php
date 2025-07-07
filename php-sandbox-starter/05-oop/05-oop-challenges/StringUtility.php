1149a8ca028c7372c2d18fef23a3c7ee9dbb0fb4
Pedrin7 <pedropaim987@gmail.com>
2025-07-07 17:09:51 -0300

    finalizando o desafio!

--- php-sandbox-starter/05-oop/05-oop-challenges/StringUtility.php
+++ php-sandbox-starter/05-oop/05-oop-challenges/StringUtility.php
@@ -2,11 +2,21 @@
 
 class StringUtility{
 
-    public static string $shout;
+   public static function shout($string){
+    $stringUpper = strtoupper($string);
+    return $stringUpper . '!';
+   }
 
-    public function __construct(){
-        
-    }
+   public static function whisper($string){
+    $stringLower = strtolower($string);
+    return $stringLower . '.';
+   }
 
-    
+   public static function repeat($string, $times = 2){
+        return str_repeat($string, $times);
+   }    
 }
+
+echo StringUtility::shout('Ola, mundo') . '<br>';
+echo StringUtility::whisper('Ola, mundo') . '<br>';
+echo ('Aqui é o método REPEAT: ' . '<br>') . StringUtility::repeat('Ola, mundo.' . '<br>', 5);
