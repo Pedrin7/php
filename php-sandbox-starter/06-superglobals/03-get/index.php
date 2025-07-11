52faa9abce9d91f63b814ee9343ed24c24519385
Pedrin7 <pedropaim987@gmail.com>
2025-07-11 15:54:38 -0300

    get data from query params

--- php-sandbox-starter/06-superglobals/03-get/index.php
+++ php-sandbox-starter/06-superglobals/03-get/index.php
@@ -1,3 +1,7 @@
 <?php
 // Add a query parameter to the URL in the browser's address bar:
 // http://localhost:8000/?name=John
+
+echo isset($_GET['name']) ? 'Pedro' : '';
+echo $_GET['name'] ?? '';
+
