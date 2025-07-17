90f8f9104ee1ac2fb0860d88709abd58e56a99f5
Pedrin7 <pedropaim987@gmail.com>
2025-07-17 16:32:33 -0300

    conectando no banco de dados

---
+++ php-sandbox-starter/07-database/07-connect-pdo/database.php
@@ -0,0 +1,23 @@
+<?php
+
+// database creds
+$host = 'localhost';
+$port = '3307';
+$dbname = 'blog';
+$username = 'root';
+$password = 'root';
+
+$dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8";
+
+try{
+    // create our PDO instance
+    $pdo = new PDO($dsn, $username, $password);
+
+// set PDO to throw exceptions on error
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo 'Database connected...';
+}catch(PDOException $e){
+    // if there is an error, catch it here
+    echo 'Connection failed: ' . $e->getMessage();
+}
