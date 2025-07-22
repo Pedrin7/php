fc0a1a82100de383aff2bf8950c3f535ac2ce8ab
Pedrin7 <pedropaim987@gmail.com>
2025-07-22 18:06:03 -0300

    criando as rotas e adequando a minha máquina, algumas rotas que eram pra dar erro não estão funcionando ainda

--- workopia/public/index.php
+++ workopia/public/index.php
@@ -2,5 +2,20 @@
 
 require '../helpers.php';
 
-loadView('home');
+$routes = [
+    '/php/workopia/public/' => 'controllers/home.php',
+    '/php/controllers/listings' => 'controllers/listings/index.php',
+    '/php/controllers/listings' => 'controllers/listings/create.php',
+    '/php/controllers/error/404' => 'controllers/error/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+// inspectAndDie($uri);
+
+if(array_key_exists($uri, $routes)){
+    require(basePath($routes[$uri]));
+} else{
+    require basePath($routes['/php/controllers/error/404']);
+}
 
