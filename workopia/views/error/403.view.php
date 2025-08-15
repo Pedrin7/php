e59f5f89743c66d70ee202816059d27108f128bf
Pedrin7 <pedropaim987@gmail.com>
2025-08-15 19:40:24 -0300

    finalizando a seção 9

---
+++ workopia/views/error/403.view.php
@@ -0,0 +1,14 @@
+<?php  require '../../helpers.php'; ?>
+
+<?= loadPartial('head') ?>
+<?= loadPartial('header.navbar') ?>
+    <section>
+      <div class="container mx-auto p-4 mt-4">
+         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">403 Error</div>
+         <p class="text-center text-2xl mb-4">
+            Acesso não autorizado
+         </p>
+      </div>
+      </section>
+
+<?= loadPartial('footer') ?>
